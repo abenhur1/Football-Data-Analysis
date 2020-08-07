f426d8ad656f6eee595fd3a0adada22aa1f15d28
User <abenhur1989@gmail.com>
2020-08-07 19:50:38 +0300

    Commit

--- ML_Functions.py
+++ ML_Functions.py
@@ -34,7 +34,23 @@ for col in X_La_Liga.columns:
 X_La_Liga_train, X_La_Liga_validation, y_La_Liga_train, y_La_Liga_validation = \
     train_test_split(X_La_Liga, y_La_Liga, test_size=0.20, random_state=1)
 
-# Parameter tuning:
+# ## Evaluate each model in turn and compare algorithms:
+# models = [('LogReg', LogisticRegression(solver='liblinear', multi_class='ovr')), ('LinDiscAnal', LinearDiscriminantAnalysis()),
+#           ('KNN', KNeighborsClassifier()), ('DeciTree', DecisionTreeClassifier()), ('GaussianNB', GaussianNB()),
+#           ('SVM', SVC(kernel='rbf', gamma='auto')), ('XGB', xgb.XGBClassifier())]
+# results = []
+# names = []
+# for name, model in models:
+#     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+#     cv_results = cross_val_score(model, X_La_Liga_train, y_La_Liga_train, cv=kfold, scoring='accuracy')
+#     results.append(cv_results)
+#     names.append(name)
+#     print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# plt.boxplot(results, labels=names)
+# plt.title('Algorithm Comparison')
+# plt.show()
+
+## Parameter tuning:
 parameters = {'learning_rate': [0.1],
               'n_estimators': [40],
               'max_depth': [3],
@@ -46,45 +62,30 @@ parameters = {'learning_rate': [0.1],
               'reg_alpha': [1e-5]
               }
 
-# Evaluate each model in turn and compare algorithms:
-models = [('LogReg', LogisticRegression(solver='liblinear', multi_class='ovr')), ('LinDiscAnal', LinearDiscriminantAnalysis()),
-          ('KNN', KNeighborsClassifier()), ('DeciTree', DecisionTreeClassifier()), ('GaussianNB', GaussianNB()),
-          ('SVM', SVC(kernel='rbf', gamma='auto')), ('XGB', xgb.XGBClassifier())]
-results = []
-names = []
-# for name, model in models:
-#     kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-#     cv_results = cross_val_score(model, X_La_Liga_train, y_La_Liga_train, cv=kfold, scoring='accuracy')
-#     results.append(cv_results)
-#     names.append(name)
-#     print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
-# plt.boxplot(results, labels=names)
-# plt.title('Algorithm Comparison')
-# plt.show()
-
 kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
 cv_results = cross_val_score(xgb.XGBClassifier(), X_La_Liga_train, y_La_Liga_train, cv=kfold, scoring='accuracy')
 print('%s: mean:%f (mean:%f)' % ('XGB', cv_results.mean(), cv_results.std()))
-# Initialize the classifier
+
+## Initialize the classifier
 clf = xgb.XGBClassifier(seed=2)
 
-# Make an f1 scoring function using 'make_scorer'
+## Make an f1 scoring function using 'make_scorer'
 f1_scorer = make_scorer(f1_score, pos_label='H')
 
-# Perform grid search on the classifier using the f1_scorer as the scoring method
+## Perform grid search on the classifier using the f1_scorer as the scoring method
 grid_obj = GridSearchCV(clf,
                         scoring=f1_scorer,
                         param_grid=parameters,
                         cv=5)
 
-# Fit the grid search object to the training data and find the optimal parameters
+## Fit the grid search object to the training data and find the optimal parameters
 grid_obj = grid_obj.fit(X_La_Liga, y_La_Liga)
 
-# Get the estimator
+## Get the estimator
 clf = grid_obj.best_estimator_
 print(clf)
 
-# Report the final F1 score for training and testing after parameter tuning
+## Report the final F1 score for training and testing after parameter tuning
 f1, acc = predict_labels(clf, X_La_Liga, y_La_Liga)
 print("F1 score and accuracy score for training set: {:.4f} , {:.4f}.".format(f1, acc))
 
