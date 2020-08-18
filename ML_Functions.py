e4533114d5205154e099b871756fecc5f0d0a9a0
User <abenhur1989@gmail.com>
2020-08-18 17:08:56 +0300

    Commit

--- ML_Functions.py
+++ ML_Functions.py
@@ -1,22 +1,15 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import xgboost as xgb
-from pandas.plotting import scatter_matrix
-# from sklearn.preprocessing import scale
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.metrics import classification_report
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import accuracy_score
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import make_scorer, f1_score
+from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+# from pandas.plotting import scatter_matrix
+# import matplotlib.pyplot as plt
 
 from Data_Cleaning_for_ML import laLiga0919FilteredML
 
@@ -24,39 +17,90 @@ pd.set_option('display.width', 400)
 pd.set_option('display.max_columns', 16)
 
 
-X_La_Liga = laLiga0919FilteredML.drop(['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR'], axis=1)
-print(X_La_Liga.head())
-y_La_Liga = laLiga0919FilteredML['FTR']
-# print(y_La_Liga.head())
-X_train, X_test, y_train, y_test = train_test_split(X_La_Liga, y_La_Liga, random_state=0)
+# Makes predictions using a fit classifier based on F1 score:
+def predict_labels(classifier, features, target):
+    y_pred = classifier.predict(features)
+
+    return f1_score(target, y_pred, average='weighted'), sum(target == y_pred) / float(len(y_pred))
+
+
+# Train and predict using a classifer based on F1 score:
+def train_predict(classifier, X_train, y_train, X_test, y_test):
+    classifier.fit(X_train, y_train)
+    # Indicate the classifier and the training set size
+    print("Training a {} using a training set size of {}. . .".format(classifier.__class__.__name__, len(X_train)))
+
+    # Print the results of prediction for both training and testing
+    f1, acc = predict_labels(classifier, X_train, y_train)
+    print(f1, acc)
+
+    print("F1 score and accuracy score (before GridSearch) for training set: {:.4f} , {:.4f}.".format(f1, acc))
+
+    f1, acc = predict_labels(classifier, X_test, y_test)
+    print("F1 score and accuracy score (before GridSearch) for test set: {:.4f} , {:.4f}.".format(f1, acc))
+
+
+X_La_Liga = laLiga0919FilteredML.drop(['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR'], axis=1).copy()
+# print('X_La_Liga head:', X_La_Liga.head())
+y_La_Liga = laLiga0919FilteredML['FTR'].copy()
+# print('y_La_Liga head:', y_La_Liga.head())
+
+# scatter = scatter_matrix(X_La_Liga[['HTAggGoalScored', 'ATAggGoalScored', 'HTAggGoalConceded', 'ATAggGoalConceded',
+#                                                 'HTAggLeaguePoints', 'ATAggLeaguePoints', 'NumOfPastHTSpecificWinsOutOf3',
+#                                                 'NumOfPastATSpecificWinsOutOf3', 'NumOfPastHTWinsOutOfLast3Matches',
+#                                                 'NumOfPastATWinsOutOfLast3Matches', 'HTWinningChancesAtHome', 'ATWinningChancesWhenAway']])
+# plt.show()
+
+X_trained, X_tested, y_trained, y_tested = train_test_split(X_La_Liga, y_La_Liga, random_state=0)
 scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_train_scaled = scaler.fit_transform(X_trained)
+X_test_scaled = scaler.transform(X_tested)
 # for col in X_La_Liga.columns:
 #     X_La_Liga[col] = scale(X_La_Liga[col])
-print(X_La_Liga.head())
-X_La_Liga_train, X_La_Liga_validation, y_La_Liga_train, y_La_Liga_validation = \
-    train_test_split(X_La_Liga, y_La_Liga, test_size=0.20, random_state=1)
 
-## Evaluate each model in turn and compare algorithms:
-models = [('LogReg', LogisticRegression(solver='liblinear', multi_class='ovr')), ('LinDiscAnal', LinearDiscriminantAnalysis()),
-          ('GaussianNB', GaussianNB()), ('SVM (kernel=rbf)', SVC(kernel='rbf', gamma='auto', random_state=1)), ('XGB', xgb.XGBClassifier(seed=2))]
-results = []
-names = []
-for name, model in models:
-    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-    cv_results = cross_val_score(model, X_train_scaled, y_La_Liga_train, cv=kfold, scoring='accuracy')
-    results.append(cv_results)
-    names.append(name)
-    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
-scatter_matrix(X_La_Liga[['HTAggGoalScored', 'ATAggGoalScored', 'HTAggGoalConceded', 'ATAggGoalConceded', 'HTAggLeaguePoints', 'ATAggLeaguePoints',
-                          'NumOfPastHTSpecificWinsOutOf3', 'NumOfPastATSpecificWinsOutOf3', 'NumOfPastHTWinsOutOfLast3Matches',
-                          'NumOfPastATWinsOutOfLast3Matches', 'HTWinningChancesAtHome', 'ATWinningChancesWhenAway']], figsize=(10, 10))
-plt.figure()
-plt.boxplot(results, labels=names)
-plt.title('Algorithm Comparison')
-plt.show()
 
-kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-cv_results = cross_val_score(xgb.XGBClassifier(), X_La_Liga_train, y_La_Liga_train, cv=kfold, scoring='accuracy')
-print('%s: mean:%f (mean:%f)' % ('XGB', cv_results.mean(), cv_results.std()))
+clf_SVC = SVC(random_state=0, kernel='rbf')
+clf_LogReg = LogisticRegression(random_state=0)
+clf_XGB = xgb.XGBClassifier(seed=0)
+clf_LinDiscriminantAnalysis = LinearDiscriminantAnalysis()
+clf_list = [clf_SVC, clf_LogReg, clf_LinDiscriminantAnalysis, clf_XGB]
+
+for clf in clf_list:
+    train_predict(clf, scaler.fit_transform(X_trained), y_trained, scaler.transform(X_tested), y_tested)
+    print('')
+
+# Create the parameters list you wish to tune
+parameters = {'learning_rate': [0.1],
+              'n_estimators': [40],
+              'max_depth': [3],
+              'min_child_weight': [3],
+              'gamma': [0.4],
+              'subsample': [0.8],
+              'colsample_bytree': [0.8],
+              'scale_pos_weight': [1],
+              'reg_alpha': [1e-5]
+              }
+
+# Initialize the classifier
+clf = xgb.XGBClassifier(seed=2)
+
+# Make an f1 scoring function using 'make_scorer'
+precision_scorer = make_scorer(precision_score, average='weighted')
+recall_scorer = make_scorer(recall_score, average='weighted')
+f1_scorer = make_scorer(f1_score, average='weighted')
+
+# Perform grid search on the classifier using the different scorers:
+for scorer in [precision_scorer, recall_scorer, f1_scorer]:
+    grid_obj = GridSearchCV(estimator=clf, param_grid=parameters, scoring=scorer, cv=5)
+    # Fit the grid search object to the training data and find the optimal parameters
+    grid_obj = grid_obj.fit(scaler.fit_transform(X_trained), y_trained)
+
+    # Get the best estimator
+    clf = grid_obj.best_estimator_
+    print(str(scorer) + ': {}'.format(clf))
+
+# Final F1 score for training and testing after GridSearch:
+f1Score, accuracy = predict_labels(clf, scaler.fit_transform(X_trained), y_trained)
+print("F1 score and accuracy score for training set: {:.4f} , {:.4f}.".format(f1Score, accuracy))
+f1Score, accuracy = predict_labels(clf, scaler.fit_transform(X_tested), y_tested)
+print("F1 score and accuracy score for test set: {:.4f} , {:.4f}.".format(f1Score, accuracy))
--- playground.py
+++ playground.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 import xgboost as xgb
-from time import time
-from sklearn.metrics import f1_score
 from pandas.plotting import scatter_matrix
 # from sklearn.preprocessing import scale
+# from sklearn.preprocessing import StandardScaler
 # from sklearn.metrics import classification_report
 # from sklearn.metrics import confusion_matrix
 # from sklearn.metrics import accuracy_score
@@ -19,89 +23,40 @@ from Data_Cleaning_for_ML import laLiga0919FilteredML
 pd.set_option('display.width', 400)
 pd.set_option('display.max_columns', 16)
 
-## for measuring training time
-# F1 score (also F-score or F-measure) is a measure of a test's accuracy.
-# It considers both the precision p and the recall r of the test to compute
-# the score: p is the number of correct positive results divided by the number of
-# all positive results, and r is the number of correct positive results divided by
-# the number of positive results that should have been returned. The F1 score can be
-# interpreted as a weighted average of the precision and recall, where an F1 score
-# reaches its best value at 1 and worst at 0.
 
-
-def train_classifier(clf, X_train, y_train):
-    """ Fits a classifier to the training data. """
-
-    # Start the clock, train the classifier, then stop the clock
-    start = time()
-    clf.fit(X_train, y_train)
-    end = time()
-
-    # Print the results
-    print("Trained model in {:.4f} seconds".format(end - start))
-
-
-def predict_labels(clf, features, target):
-    """ Makes predictions using a fit classifier based on F1 score. """
-
-    # Start the clock, make predictions, then stop the clock
-    start = time()
-    y_pred = clf.predict(features)
-
-    end = time()
-    # Print and return results
-    print("Made predictions in {:.4f} seconds.".format(end - start))
-
-    return f1_score(target, y_pred, average='micro'), sum(target == y_pred) / float(len(y_pred))
-
-
-def train_predict(clf, X_train, y_train, X_test, y_test):
-    """ Train and predict using a classifer based on F1 score. """
-
-    # Indicate the classifier and the training set size
-    print("Training a {} using a training set size of {}. . .".format(clf.__class__.__name__, len(X_train)))
-
-    # Train the classifier
-    train_classifier(clf, X_train, y_train)
-
-    # Print the results of prediction for both training and testing
-    f1, acc = predict_labels(clf, X_train, y_train)
-    print(f1, acc)
-    print("F1 score and accuracy score for training set: {:.4f} , {:.4f}.".format(f1, acc))
-
-    f1, acc = predict_labels(clf, X_test, y_test)
-    print("F1 score and accuracy score for test set: {:.4f} , {:.4f}.".format(f1, acc))
-
-
-X_La_Liga = laLiga0919FilteredML.drop(['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR'], axis=1).copy()
-# print('X_La_Liga head:', X_La_Liga.head())
-y_La_Liga = laLiga0919FilteredML['FTR'].copy()
-# print('y_La_Liga head:', y_La_Liga.head())
-
-scatter_matrix(X_La_Liga[['HTAggGoalScored', 'ATAggGoalScored', 'HTAggGoalConceded', 'ATAggGoalConceded', 'HTAggLeaguePoints', 'ATAggLeaguePoints',
-                          'NumOfPastHTSpecificWinsOutOf3', 'NumOfPastATSpecificWinsOutOf3', 'NumOfPastHTWinsOutOfLast3Matches',
-                          'NumOfPastATWinsOutOfLast3Matches', 'HTWinningChancesAtHome', 'ATWinningChancesWhenAway']])
-
-X_trained, X_tested, y_trained, y_tested = train_test_split(X_La_Liga, y_La_Liga, random_state=0)
+X_La_Liga = laLiga0919FilteredML.drop(['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR'], axis=1)
+print(X_La_Liga.head())
+y_La_Liga = laLiga0919FilteredML['FTR']
+# print(y_La_Liga.head())
+X_train, X_test, y_train, y_test = train_test_split(X_La_Liga, y_La_Liga, random_state=0)
 scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_trained)
-X_test_scaled = scaler.transform(X_tested)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 # for col in X_La_Liga.columns:
 #     X_La_Liga[col] = scale(X_La_Liga[col])
 print(X_La_Liga.head())
-print(X_trained)
+X_La_Liga_train, X_La_Liga_validation, y_La_Liga_train, y_La_Liga_validation = \
+    train_test_split(X_La_Liga, y_La_Liga, test_size=0.20, random_state=1)
 
+## Evaluate each model in turn and compare algorithms:
+models = [('LogReg', LogisticRegression(solver='liblinear', multi_class='ovr')), ('LinDiscAnal', LinearDiscriminantAnalysis()),
+          ('GaussianNB', GaussianNB()), ('SVM (kernel=rbf)', SVC(kernel='rbf', gamma='auto', random_state=1)), ('XGB', xgb.XGBClassifier(seed=2))]
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train_scaled, y_La_Liga_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+scatter_matrix(X_La_Liga[['HTAggGoalScored', 'ATAggGoalScored', 'HTAggGoalConceded', 'ATAggGoalConceded', 'HTAggLeaguePoints', 'ATAggLeaguePoints',
+                          'NumOfPastHTSpecificWinsOutOf3', 'NumOfPastATSpecificWinsOutOf3', 'NumOfPastHTWinsOutOfLast3Matches',
+                          'NumOfPastATWinsOutOfLast3Matches', 'HTWinningChancesAtHome', 'ATWinningChancesWhenAway']], figsize=(10, 10))
+plt.figure()
+plt.boxplot(results, labels=names)
+plt.title('Algorithm Comparison')
+plt.show()
 
-# Initialize the three models (XGBoost is initialized later)
-clf_A = LogisticRegression(random_state=42)
-clf_B = SVC(random_state=912, kernel='rbf')
-# Boosting refers to this general problem of producing a very accurate prediction rule
-# by combining rough and moderately inaccurate rules-of-thumb
-clf_C = xgb.XGBClassifier(seed=82)
-
-train_predict(clf_A, scaler.fit_transform(X_trained), y_trained, scaler.transform(X_tested), y_tested)
-print('')
-train_predict(clf_B, scaler.fit_transform(X_trained), y_trained, scaler.transform(X_tested), y_tested)
-print('')
-train_predict(clf_C, scaler.fit_transform(X_trained), y_trained, scaler.transform(X_tested), y_tested)
-print('')
+kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+cv_results = cross_val_score(xgb.XGBClassifier(), X_La_Liga_train, y_La_Liga_train, cv=kfold, scoring='accuracy')
+print('%s: mean:%f (mean:%f)' % ('XGB', cv_results.mean(), cv_results.std()))
